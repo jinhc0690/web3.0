b303096dbf9ff60b319ffbf45af7f066445efadd
jinhc0690 <121592562+jinhc0690@users.noreply.github.com>
2025-07-31 21:52:41 +0800

    Update main.go

--- project/16合约代码生成/main.go
+++ project/16合约代码生成/main.go
@@ -15,6 +15,20 @@ import (
 )
 
 func main() {
+
+	// 任务 2：合约代码生成 任务目标
+	// 使用 abigen 工具自动生成 Go 绑定代码，用于与 Sepolia 测试网络上的智能合约进行交互。
+	//  具体任务
+	// 编写智能合约
+	// 使用 Solidity 编写一个简单的智能合约，例如一个计数器合约。
+	// 编译智能合约，生成 ABI 和字节码文件。
+	// 使用 abigen 生成 Go 绑定代码
+	// 安装 abigen 工具。
+	// 使用 abigen 工具根据 ABI 和字节码文件生成 Go 绑定代码。
+	// 使用生成的 Go 绑定代码与合约交互
+	// 编写 Go 代码，使用生成的 Go 绑定代码连接到 Sepolia 测试网络上的智能合约。
+	// 调用合约的方法，例如增加计数器的值。
+	// 输出调用结果。
 	client, err := ethclient.Dial("https://sepolia.infura.io/v3/5b20ca5b9ddf4b4c851351696ad9d564")
 	if err != nil {
 		log.Fatal(err)
